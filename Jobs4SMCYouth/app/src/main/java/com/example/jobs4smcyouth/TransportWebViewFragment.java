5f87e4080307d5371be925a4cbff2e222525439b
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-05-21 16:13:12 -0700

    Finished transportation fragment which includes webviewfragment of googledoc form

--- Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/TransportWebViewFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/TransportWebViewFragment.java
@@ -9,19 +9,16 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.example.jobs4smcyouth.R;
-
-import java.net.URL;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LaborLawsWebviewFragment extends Fragment {
-    private final String URL = "http://www.dir.ca.gov/dlse/MinorsSummaryCharts.pdf";
+public class TransportWebViewFragment extends Fragment {
+    private final String URL = "https://docs.google.com/spreadsheets/d/1f6V2nEdJ4HuXhshnqMPJfHyRtLxc-ivOBOXT2BjYYeo/edit#gid=1990877982";
     WebView webView;
     CustomWebViewClient customWebViewClient;
 
-    public LaborLawsWebviewFragment() {
+    public TransportWebViewFragment() {
         // Required empty public constructor
     }
 
@@ -30,15 +27,15 @@ public class LaborLawsWebviewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_labor_laws_webview, container, false);
+        View v = inflater.inflate(R.layout.fragment_transport_web_view, container, false);
         webView = (WebView) v.findViewById(R.id.laborLaws_webView_id);
         webView.loadUrl(URL);
         customWebViewClient = new CustomWebViewClient();
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(customWebViewClient);
         return v;
-    }
 
+    }
 
     private class CustomWebViewClient extends WebViewClient {
         @Override
@@ -48,5 +45,3 @@ public class LaborLawsWebviewFragment extends Fragment {
     }
 
 }
-
-// <url of webview fragment
