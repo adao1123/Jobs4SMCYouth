0507e5277c07f548b6407160838c74b0f15b241e
adao1123 <adao1193@gmail.com>
2016-05-21 10:37:07 -0700

    made drawer

--- Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/MainActivity.java
+++ Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.jobs4smcyouth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+private LinearLayout drawerLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLinearLayout = (LinearLayout)findViewById(R.id.drawer_linear_layout_id);
+        
     }
 }
