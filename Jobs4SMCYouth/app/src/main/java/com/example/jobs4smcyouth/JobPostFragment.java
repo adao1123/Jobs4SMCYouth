eb2328ea9f989df78d28d6f5834da16340185647
adao1123 <adao1193@gmail.com>
2016-05-21 11:09:31 -0700

    init firebase

--- Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/JobPostFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/JobPostFragment.java
@@ -7,12 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.client.Firebase;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class JobPostFragment extends Fragment {
-
+    private Firebase firebaseListings;
 
     public JobPostFragment() {
         // Required empty public constructor
@@ -20,10 +22,24 @@ public class JobPostFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_job_post, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        handleFirebase();
+    }
+
+    private void handleFirebase(){
+        Firebase firebaseRef = new Firebase("https://jobs-4-smc-youth.firebaseio.com/");
+        firebaseListings = firebaseRef.child("Job Listings");
+    }
+
+    private void addListingsToFb(){
+        
+    }
 }
