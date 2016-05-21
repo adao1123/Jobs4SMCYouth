efdee295774f3eb22c1e98396629d07ffd8acdcf
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-05-21 13:27:02 -0700

    Finished requirements java

--- Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/RequirementFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/RequirementFragment.java
@@ -3,15 +3,25 @@ package com.example.jobs4smcyouth;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import layout.LaborLawsWebviewFragment;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RequirementFragment extends Fragment {
+    Button laborLawsButton;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    LaborLawsWebviewFragment laborLawsWebviewFragment;
+
 
 
     public RequirementFragment() {
@@ -23,11 +33,18 @@ public class RequirementFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_requirement, container, false);
-
+        laborLawsButton = (Button) v.findViewById(R.id.requirements_lawButton_id);
+        laborLawsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                laborLawsWebviewFragment = new LaborLawsWebviewFragment();
+                fragmentManager = getFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container_id, laborLawsWebviewFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
         return v;
     }
-
 }
-
-
-//http://www.dir.ca.gov/dlse/MinorsSummaryCharts.pdf <url of webview fragment
