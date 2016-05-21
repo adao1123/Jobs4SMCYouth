4383cb5d61b127fb7c89a6e6afe162eecfba5a4c
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-05-21 12:49:27 -0700

    Created requirements xml finished both cards

--- Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/RequirementFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/RequirementFragment.java
@@ -22,8 +22,12 @@ public class RequirementFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_requirement, container, false);
+        View v = inflater.inflate(R.layout.fragment_requirement, container, false);
+
+        return v;
     }
 
 }
+
+
+//http://www.dir.ca.gov/dlse/MinorsSummaryCharts.pdf <url of webview fragment
