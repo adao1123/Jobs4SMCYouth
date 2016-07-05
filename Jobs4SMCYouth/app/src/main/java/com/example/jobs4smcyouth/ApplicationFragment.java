6a6d589ff108cd66c7ea456845bd16b55855bb00
Sam Siu <samsiu@Sams-MacBook-Pro.local>
2016-07-05 14:55:15 -0700

    ApplicationFragment: Implemented RecyclerView

--- Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/ApplicationFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/ApplicationFragment.java
@@ -3,14 +3,11 @@ package com.example.jobs4smcyouth;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import com.example.jobs4smcyouth.Utilities.TouchImageView;
-import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,14 +29,7 @@ public class ApplicationFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_application, container, false);
 
-        TouchImageView masterForm1ImageView = (TouchImageView)view.findViewById(R.id.application_masterForm1_imageView);
-        TouchImageView masterForm2ImageView = (TouchImageView)view.findViewById(R.id.application_masterForm2_imageView);
-        TouchImageView volunteerForm1ImageView = (TouchImageView)view.findViewById(R.id.application_sampleVolunteerForm1_imageView);
-        TouchImageView volunteerForm2ImageView = (TouchImageView)view.findViewById(R.id.application_sampleVolunteerForm2_imageView);
-        TouchImageView noExperienceForm1ImageView = (TouchImageView)view.findViewById(R.id.application_noExperienceForm1_imageView);
-        TouchImageView noExperienceForm2ImageView = (TouchImageView)view.findViewById(R.id.application_noExperienceForm2_imageView);
-        TouchImageView jobExperienceForm1ImageView = (TouchImageView)view.findViewById(R.id.application_jobExperienceForm1_imageView);
-        TouchImageView jobExperienceForm2ImageView = (TouchImageView)view.findViewById(R.id.application_jobExperienceForm2_imageView);
+        RecyclerView applicationRecyclerView = (RecyclerView)view.findViewById(R.id.applicationFrag_recyclerView);
 
         List<String> links = new ArrayList<String>();
         links.add("http://i.imgur.com/OlLu3bc.png"); // MasterApplication Page 1
@@ -51,23 +41,10 @@ public class ApplicationFragment extends Fragment {
         links.add("http://i.imgur.com/zoqznjx.png"); // SampleJobExperience1
         links.add("http://i.imgur.com/oKi8Mk4.png"); // SampleJobExperience2
 
-        List<ImageView> imageViews = new ArrayList<ImageView>();
-        imageViews.add(masterForm1ImageView);
-        imageViews.add(masterForm2ImageView);
-        imageViews.add(volunteerForm1ImageView);
-        imageViews.add(volunteerForm2ImageView);
-        imageViews.add(noExperienceForm1ImageView);
-        imageViews.add(noExperienceForm2ImageView);
-        imageViews.add(jobExperienceForm1ImageView);
-        imageViews.add(jobExperienceForm2ImageView);
-
-        for(int i = 0; i < imageViews.size(); i++){
-            Picasso.with(getContext())
-                    .load(links.get(i))
-                    .resize(1000, 1700)
-                    .into(imageViews.get(i));
-        }
-
+        ApplicationFragmentRVAdapter applicationFragmentRVAdapter = new ApplicationFragmentRVAdapter(links);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL, false);
+        applicationRecyclerView.setLayoutManager(linearLayoutManager);
+        applicationRecyclerView.setAdapter(applicationFragmentRVAdapter);
 
         return view;
     }
