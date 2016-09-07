188757df39444eb3aee772c6359dc5edaae215e4
Sam Siu <samsiu@Sams-MacBook-Pro-2.local>
2016-09-06 18:39:14 -0700

    About: Added Markers and Info to Maps

--- Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/Fragments/AboutFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/Fragments/AboutFragment.java
@@ -1,15 +1,21 @@
 package com.example.jobs4smcyouth.Fragments;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.jobs4smcyouth.Models.AboutLocation;
 import com.example.jobs4smcyouth.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,24 +24,49 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+
+271 92nd Street
+Daily City, CA 94015
+37.6894381,-122.4740778
+
+
+789 E. Poplar Ave
+San Mateo, CA 94401
+37.577871,-122.3284768
+
+
+400 Harbor Blvd Building B
+Belmont, CA 94002
+37.520316,-122.2693028
+
+2500 Middlefield Road
+Redwood City, CA 94063
+37.477371,-122.2142568
+
+ */
+
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutFragment extends Fragment implements OnMapReadyCallback{
-    private TextView phoneDCtextView;
-    private TextView phoneAdulttextView;
+    private TextView phoneDCTextView;
+    private TextView phoneAdultTextView;
     private TextView phoneBelmontTextView;
     private TextView phoneRedwoodTextView;
-    private String phoneNumber;
 
     private GoogleMap map;
     private static String latitude;
     private static String longitude;
+    private List<AboutLocation> officeLocations;
 
     public AboutFragment() {
         // Required empty public constructor
@@ -48,49 +79,82 @@ public class AboutFragment extends Fragment implements OnMapReadyCallback{
         View v = inflater.inflate(R.layout.fragment_about, container, false);
 
         initializeViews(v);
-
+        initilizeLocations();
         initGoogleMaps();
 
-        phoneDCtextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                phoneNumber = "6503018434";
-                dialPhoneNumber(phoneNumber);
-            }
-        });
 
-        phoneAdulttextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                phoneNumber = "6503018434";
-                dialPhoneNumber(phoneNumber);
-            }
-        });
-
-        phoneBelmontTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                phoneNumber = "6508026534";
-                dialPhoneNumber(phoneNumber);
-            }
-        });
-
-        phoneRedwoodTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                phoneNumber = "6508026534";
-                dialPhoneNumber(phoneNumber);
-            }
-        });
+        setDialNumberListener(phoneDCTextView, "6503018434");
+        setDialNumberListener(phoneAdultTextView, "6503018434");
+        setDialNumberListener(phoneBelmontTextView, "6508026534");
+        setDialNumberListener(phoneRedwoodTextView, "6508026534");
 
         return v;
     }
 
     private void initializeViews(View v){
-        phoneDCtextView = (TextView) v.findViewById(R.id.about_phoneDC_tV_id);
-        phoneAdulttextView = (TextView) v.findViewById(R.id.about_phoneAdultSchool_tV_id);
+        phoneDCTextView = (TextView) v.findViewById(R.id.about_phoneDC_tV_id);
+        phoneAdultTextView = (TextView) v.findViewById(R.id.about_phoneAdultSchool_tV_id);
         phoneBelmontTextView = (TextView) v.findViewById(R.id.about_phoneBelmont_tV_id);
         phoneRedwoodTextView = (TextView) v.findViewById(R.id.about_phoneRedwood_tV_id);
+    }
+
+
+    private void initilizeLocations(){
+
+        AboutLocation dalyCityLocation = new AboutLocation(
+                37.6894381,
+                -122.4740778,
+                "San Mateo County Human Services Agency", "271 92nd Street \nDaly City, CA 94015",
+                "",
+                "(650)301-8434", // Phone
+                "(650)757-3224"  // Fax
+        );
+
+        AboutLocation sanMateoLocation = new AboutLocation(
+                37.577871,
+                -122.3284768,
+                "San Mateo Adult School \n(Satellite Office)",
+                "789 E. Poplar Ave \nSan Mateo, CA 94401",
+                "No Representative On-Site",
+                "(650)301-8434",
+                "N/A"
+        );
+
+        AboutLocation belmontLocation = new AboutLocation(
+                37.520316,
+                -122.2693028,
+                "San Mateo County Human Services Agency",
+                "400 Harbor Blvd Building B \nBelmont, CA 94002",
+                "No Representative On-Site",
+                "(650)802-6534",
+                "(650)508-0782"
+        );
+
+        AboutLocation redwoodLocation = new AboutLocation(
+                37.477371,
+                -122.2142568,
+                "San Mateo County Human Services Agency \n(Satellite Office)",
+                "2500 Middlefield Road \nRedwood City, CA 94063",
+                "No Representative On-Site",
+                "(650)802-6534",
+                "N/A"
+        );
+
+        officeLocations = new ArrayList<>();
+        officeLocations.add(dalyCityLocation);
+        officeLocations.add(sanMateoLocation);
+        officeLocations.add(belmontLocation);
+        officeLocations.add(redwoodLocation);
+
+    }
+
+    private void setDialNumberListener(TextView textView, final String phoneNumber){
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialPhoneNumber(phoneNumber);
+            }
+        });
     }
 
     public void dialPhoneNumber(String phoneNumber) {
@@ -110,30 +174,83 @@ public class AboutFragment extends Fragment implements OnMapReadyCallback{
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-        latitude = "37.7157584";
-        longitude = "-122.2140615";
+        latitude = "37.577871";
+        longitude = "-122.3284768";
         setGoogleMapCameraPosition(Double.parseDouble(latitude), Double.parseDouble(longitude));
-        plotGoogleMapMarker(Double.parseDouble(latitude), Double.parseDouble(longitude));
-    }
+        plotSMCLocations();
 
+    }
 
     private void setGoogleMapCameraPosition(double latitude, double longitude){
         CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(new LatLng(latitude, longitude)).zoom(10).build();
+                .target(new LatLng(latitude, longitude)).zoom(9).build();
         map.animateCamera(CameraUpdateFactory
                 .newCameraPosition(cameraPosition));
     }
 
-    private void plotGoogleMapMarker(double latitude, double longitude){
+    private void plotSMCLocations(){
+        for(AboutLocation location : officeLocations){
+            String snippet = "Address: " + location.getAddress() +
+                    "\nNotes: " + location.getNotes() +
+                    "\nPhone: " + location.getPhoneNumber() +
+                    "\nFax: " + location.getFaxNumber();
+
+            plotGoogleMapMarker(
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    location.getName(),
+                    snippet
+            );
+        }
+    }
+
+    private void plotGoogleMapMarker(double latitude, double longitude, String title, String Snippet){
+
+        setGoogleMarkerInfoTab();
+
         MarkerOptions marker = new MarkerOptions().position(
                 new LatLng(latitude, longitude))
-                .title("Address")
-                .snippet("Snippet");
+                .title(title)
+                .snippet(Snippet);
 
         // Changing marker icon
         marker.icon(BitmapDescriptorFactory
                 .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
 
         map.addMarker(marker);
+    }
+
+    private void setGoogleMarkerInfoTab(){
+        map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                Context context = getActivity().getApplicationContext();
+
+                LinearLayout info = new LinearLayout(context);
+                info.setOrientation(LinearLayout.VERTICAL);
+
+                TextView title = new TextView(context);
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+                TextView snippet = new TextView(context);
+                snippet.setTextColor(Color.GRAY);
+                snippet.setText(marker.getSnippet());
+
+                info.addView(title);
+                info.addView(snippet);
+
+                return info;
+            }
+        });
     }
 }
---
+++ Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/Models/AboutLocation.java
@@ -0,0 +1,53 @@
+package com.example.jobs4smcyouth.Models;
+
+/**
+ * Created by samsiu on 9/6/16.
+ */
+public class AboutLocation {
+
+    private Double latitude;
+    private Double longitude;
+    private String name;
+    private String address;
+    private String notes;
+    private String faxNumber;
+    private String phoneNumber;
+
+    public AboutLocation(Double latitude, Double longitude, String name, String address, String notes, String faxNumber, String phoneNumber) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.name = name;
+        this.address = address;
+        this.notes = notes;
+        this.faxNumber = faxNumber;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
