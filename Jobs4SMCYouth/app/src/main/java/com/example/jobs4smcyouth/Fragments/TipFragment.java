3c62ef7da4dca6b79497ebb3600b1f7163bf2226
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-09-09 21:53:00 -0700

    Fixed collapsing and expanding image toolbar

--- Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/Fragments/TipFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/example/jobs4smcyouth/Fragments/TipFragment.java
@@ -2,6 +2,7 @@ package com.example.jobs4smcyouth.Fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,8 +15,6 @@ import com.example.jobs4smcyouth.R;
  * A simple {@link Fragment} subclass.
  */
 public class TipFragment extends Fragment {
-
-
     public TipFragment() {
         // Required empty public constructor
     }
@@ -25,7 +24,8 @@ public class TipFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tip, container, false);
+        View v = inflater.inflate(R.layout.fragment_tip, container, false);
+        return v;
     }
 
 }
