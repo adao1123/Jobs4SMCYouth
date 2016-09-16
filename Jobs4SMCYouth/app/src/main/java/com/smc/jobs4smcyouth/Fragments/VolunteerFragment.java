5fb914e6802f6ac70c01afc385913c76a62ee2c0
Cherence Flores <leisforkokomo@CHERENCE.local>
2016-09-16 00:40:31 -0700

    Worked on Volunteer fragment java and xml

--- Jobs4SMCYouth/app/src/main/java/com/smc/jobs4smcyouth/Fragments/VolunteerFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/smc/jobs4smcyouth/Fragments/VolunteerFragment.java
@@ -3,16 +3,25 @@ package com.smc.jobs4smcyouth.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.client.Firebase;
 import com.smc.jobs4smcyouth.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class VolunteerFragment extends Fragment {
+    private static final String TAG = "VOLUNTEER POST FRAGMENT";
+    private Firebase firebaseListings;
+    private RecyclerView volunteerListingRV;
+    private LinearLayoutManager linearLayoutManager;
+    private GridLayoutManager gridLayoutManager;
 
 
     public VolunteerFragment() {
