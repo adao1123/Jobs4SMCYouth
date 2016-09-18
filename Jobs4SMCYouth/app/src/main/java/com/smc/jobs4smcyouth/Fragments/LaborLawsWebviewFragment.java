e7308764bfdbcb4614eaadc854ce90be8d87e335
Sam Siu <samsiu@iPhone.attlocal.net>
2016-09-18 11:54:16 -0700

    LaborLawsWebviewFragment: Added Analytics

--- Jobs4SMCYouth/app/src/main/java/com/smc/jobs4smcyouth/Fragments/LaborLawsWebviewFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/smc/jobs4smcyouth/Fragments/LaborLawsWebviewFragment.java
@@ -2,6 +2,7 @@ package com.smc.jobs4smcyouth.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,19 +11,49 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import com.smc.jobs4smcyouth.MyApplication;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LaborLawsWebviewFragment extends Fragment {
     private final String URL = "http://www.dir.ca.gov/dlse/MinorsSummaryCharts.pdf";
+    private static final String TAG = LaborLawsWebviewFragment.class.getSimpleName();
+
     WebView webView;
     CustomWebViewClient customWebViewClient;
+
+    Tracker analyticsTracker;
 
     public LaborLawsWebviewFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
 
+        MyApplication application = (MyApplication) getActivity().getApplication();
+        analyticsTracker = application.getDefaultTracker();
+        sendScreenImageName();
+    }
+
+    private void sendScreenImageName(){
+        String name = TAG;
+        // [START screen_view_hit]
+        Log.i(TAG, "Setting screen name: " + name);
+        analyticsTracker.setScreenName("Screen~" + "LaborLawsWebviewFragment");
+        analyticsTracker.send(new HitBuilders.ScreenViewBuilder().build());
+        // [END screen_view_hit]
+
+        analyticsTracker.send(new HitBuilders.EventBuilder()
+                .setCategory("Action")
+                .setAction("Share")
+                .build());
+    }
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
