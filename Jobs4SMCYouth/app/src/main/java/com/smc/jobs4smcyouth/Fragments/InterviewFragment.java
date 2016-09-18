7da0970cd2d08c9e3c07bc89646eba1d27a41d20
Sam Siu <samsiu@iPhone.attlocal.net>
2016-09-18 11:44:01 -0700

    Interview: Added Analytics

--- Jobs4SMCYouth/app/src/main/java/com/smc/jobs4smcyouth/Fragments/InterviewFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/smc/jobs4smcyouth/Fragments/InterviewFragment.java
@@ -2,13 +2,18 @@ package com.smc.jobs4smcyouth.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import com.smc.jobs4smcyouth.MyApplication;
 import com.smc.jobs4smcyouth.R;
 
 
@@ -16,6 +21,8 @@ import com.smc.jobs4smcyouth.R;
  * A simple {@link Fragment} subclass.
  */
 public class InterviewFragment extends Fragment {
+
+    private static final String TAG = InterviewFragment.class.getSimpleName();
     private CardView beforeCard;
     private CardView duringCard;
     private CardView afterCard;
@@ -23,8 +30,35 @@ public class InterviewFragment extends Fragment {
     private TextView duringDetail;
     private TextView afterDetail;
 
+    private Tracker analyticsTracker;
+
     public InterviewFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MyApplication application = (MyApplication) getActivity().getApplication();
+        analyticsTracker = application.getDefaultTracker();
+        sendScreenImageName();
+
+    }
+
+    private void sendScreenImageName() {
+
+        String name = TAG;
+        // [START screen_view_hit]
+        Log.i(TAG, "Setting screen name: " + name);
+        analyticsTracker.setScreenName("Screen~" + "AboutFragment");
+        analyticsTracker.send(new HitBuilders.ScreenViewBuilder().build());
+        // [END screen_view_hit]
+
+        analyticsTracker.send(new HitBuilders.EventBuilder()
+                .setCategory("Action")
+                .setAction("Share")
+                .build());
     }
 
     @Override
