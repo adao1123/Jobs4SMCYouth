70e98c89f55c61c35690a7a7418af7ddd20932a8
Sam Siu <samsiu@sams-mbp-2.attlocal.net>
2016-09-18 15:14:28 -0700

    Requirements: Added Animations to CardView

--- Jobs4SMCYouth/app/src/main/java/com/smc/jobs4smcyouth/Fragments/RequirementFragment.java
+++ Jobs4SMCYouth/app/src/main/java/com/smc/jobs4smcyouth/Fragments/RequirementFragment.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
@@ -29,6 +34,13 @@ public class RequirementFragment extends Fragment {
     LaborLawsWebviewFragment laborLawsWebviewFragment;
 
     private Tracker analyticsTracker;
+
+    private CardView requirement1CardView;
+    private LinearLayout requirementDetailLinearLayout;
+
+    private Animation slideUp;
+    private Animation slideDown;
+
 
     public RequirementFragment() {
         // Required empty public constructor
@@ -61,8 +73,13 @@ public class RequirementFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_requirement, container, false);
-        laborLawsButton = (Button) v.findViewById(R.id.requirements_lawButton_id);
+        View view = inflater.inflate(R.layout.fragment_requirement, container, false);
+
+        initializeViews(view);
+        loadAnimation();
+        expandDetails();
+
+        laborLawsButton = (Button) view.findViewById(R.id.requirements_lawButton_id);
         laborLawsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,6 +91,33 @@ public class RequirementFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
-        return v;
+        return view;
     }
+
+    private void initializeViews(View view){
+        requirement1CardView = (CardView)view.findViewById(R.id.requirements_workPermitCard_id);
+        requirementDetailLinearLayout = (LinearLayout)view.findViewById(R.id.requirement_permit_detail_linearLayout);
+        requirementDetailLinearLayout.setVisibility(View.GONE);
+    }
+
+    private void loadAnimation(){
+        slideUp = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_up);
+        slideDown = AnimationUtils.loadAnimation(getActivity(), R.anim.slide_down);
+    }
+
+    private void expandDetails(){
+        requirement1CardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(requirementDetailLinearLayout.getVisibility() == View.GONE){
+                    requirementDetailLinearLayout.setVisibility(View.VISIBLE);
+                    requirementDetailLinearLayout.startAnimation(slideDown);
+                }else{
+                    requirementDetailLinearLayout.setVisibility(View.GONE);
+                    requirementDetailLinearLayout.setAnimation(slideUp);
+                }
+            }
+        });
+    }
+
 }
